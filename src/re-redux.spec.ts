ebd863f27a41e4817d7bf893d806dfc33f04edf0
Wickramaranga Abeygunawardhana <w@umstek.tk>
2021-03-28 11:26:53 +0530

    Unify tests

--- src/re-redux.spec.ts
+++ src/re-redux.spec.ts
@@ -1,60 +1,6 @@
 import { createStore } from './re-redux';
 import { Action, Store } from './types';
 
-function counterReducer(state = { value: 0 }, action: Action) {
-  switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 };
-    case 'counter/decremented':
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+import createTests from './unified-tests';
 
-describe('redux basics', () => {
-  let store: Store<{ value: number }, Action<string>>;
-
-  beforeEach(() => {
-    store = createStore(counterReducer);
-  });
-
-  test('dispatch incremented', () => {
-    store.dispatch({ type: 'counter/incremented' });
-    const state = store.getState();
-    expect(state).toEqual({ value: 1 });
-  });
-
-  test('dispatch decremented', () => {
-    store.dispatch({ type: 'counter/decremented' });
-    const state = store.getState();
-    expect(state).toEqual({ value: -1 });
-  });
-
-  test('initial state', () => {
-    const state = store.getState();
-    expect(state).toEqual({ value: 0 });
-  });
-
-  test('subscription', () => {
-    const mockF1 = jest.fn();
-    const mockF2 = jest.fn();
-
-    const unsubscribe1 = store.subscribe(mockF1);
-    expect(mockF1).not.toHaveBeenCalled();
-
-    store.dispatch({ type: 'counter/incremented' });
-    expect(mockF1).toHaveBeenCalledTimes(1);
-    expect(mockF2).not.toHaveBeenCalled();
-
-    const unsubscribe2 = store.subscribe(mockF2);
-    store.dispatch({ type: 'counter/decremented' });
-    expect(mockF1).toHaveBeenCalledTimes(2);
-    expect(mockF2).toHaveBeenCalledTimes(1);
-
-    unsubscribe1();
-    store.dispatch({ type: 'counter/incremented' });
-    expect(mockF1).toHaveBeenCalledTimes(2); // mockF1 shouldn't be called.
-    expect(mockF2).toHaveBeenCalledTimes(2);
-  });
-});
+createTests(createStore);
--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,10 @@ export interface Action<T = any> {
   type: T;
 }
 
-export interface Store<S = any, A extends Action<any> = Action<any>> {
+export interface Store<
+  S = any,
+  A extends Action = Action & { [x: string]: any }
+> {
   getState: () => S;
   dispatch: (action: A) => void;
   subscribe: (listener: () => void) => () => void;
